bee73b2fd1c74acf38d1ff9964001efa81dea561
Jakub-Badzioch <jakub.badzioch.main@gmail.com>
2022-05-18 12:57:18 +0200

    init

---
+++ src/main/java/com/towerbuilder/proposalsubmitter/ProposalSubmitterApplication.java
@@ -0,0 +1,13 @@
+package com.towerbuilder.proposalsubmitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProposalSubmitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProposalSubmitterApplication.class, args);
+	}
+
+}
